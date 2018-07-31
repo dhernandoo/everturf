a8e1ee1952118a2f8510ea1625bb747ae103da11
Diego Hernando <dhernando.dev@gmail.com>
2018-07-30 23:37:46 -0400

    Update app.js to serve static files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // server/app.js
 
 /** require dependencies */
+const path = require('path');
 const express = require("express")
 const routes = require('./routes/')
 const mongoose = require('mongoose')
@@ -24,12 +25,12 @@ cloudinary.config({
 try {
     mongoose.connect(url, {
         //useMongoClient: true
-    })    
+    })
 } catch (error) {
-    
+
 }
 
-let port = 5000 || process.env.PORT
+let port = 8080 || process.env.PORT
 
 /** set up routes {API Endpoints} */
 routes(router)
@@ -38,9 +39,15 @@ routes(router)
 app.use(cors())
 app.use(bodyParser.json())
 app.use(helmet())
-//app.use('/static',express.static(path.join(__dirname,'static')))
+
+app.use('/assets', express.static(path.join(__dirname, '../build/assets')));
+app.use('/static', express.static(path.join(__dirname, '../build/static')));
 
 app.use('/api', router)
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 /** start server */
 app.listen(port, () => {
